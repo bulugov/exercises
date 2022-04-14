5da2ebb1f87be6d7ebb330ec5a78ecc9efa3135e
Bakhodir Ulugov <101068416+bulugov@users.noreply.github.com>
2022-04-14 18:20:36 +0400

    Add files via upload

--- 1/functions.py
+++ 1/functions.py
@@ -1,45 +1,60 @@
-def count_lines(ignore_whitespace):
+def count_lines(ignore_whitespace=False):
     # Count the number of non-empty lines in the text file
     file = open("poem.txt", "r")
-    for line in file:
-        totalLines = 0
-        if ignore_whitespace == True:
+    totalNonEmpty = 0
+    totalEmpty = 0
+    if ignore_whitespace == True:
+        for line in file:
+            # if ignore_whitespace == True:
             if line != "\n":
                 nonEmptyLines = line.strip("\n")
-                totalLines = len(nonEmptyLines)
+                totalNonEmpty = len(nonEmptyLines)
             else:
-                totalLines = len(file.readlines())
+                totalEmpty += 1
 
-    print("Lines:", totalLines)
+        totalEmpty += totalNonEmpty
+
+        # totalLines2 = len(file.readlines())
+
+    print("Number of lines:", totalEmpty)
+    print("Number of lines(non-empty):", totalNonEmpty)
     # Close and reopen the file after each function so one function does not affect the output of the others
     file.close()
 
 
-def count_words(ignore_whitespace):
+def count_words(ignore_whitespace=False):
     # Count the words in the text file
     file = open("poem.txt", "r")
-    totalWords = 0
+    # data = file.read()
+    nonEmptyWords = 0
     for line in file:
         if ignore_whitespace == True:
             line = line.strip("\n")
             words = line.split()
-            totalWords += len(words)
-        else:
-            words = line.split()
-            totalWords += len(words)
+            nonEmptyWords += len(words)
 
-    print("Words:", totalWords)
+    print("Words(non-empty):", nonEmptyWords)
+    print("Words:", nonEmptyWords)
     file.close()
 
 
-def count_characters(ignore_whitespace):
+def count_characters(ignore_whitespace=False):
     # Count the characters in the text file
     file = open("poem.txt", "r")
-    totalChars = 0
-    for line in file:
+    data = file.read()
+    emptyChars = 0
+    nonEmptyChars = 0
+    for line in data:
         if ignore_whitespace == True:
             line = line.strip("\n")
-            totalChars += len(line)
-        else:
-            totalChars += len(line)
-    print("Chars:", totalChars)
+            nonEmptyChars += len(line)
+
+    emptyChars += len(data)
+
+    print("Chars:", emptyChars)
+    print("Chars(non-empty):", nonEmptyChars)
+
+
+count_lines(True)
+count_characters(True)
+count_words(True)
