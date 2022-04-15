9998816657b086ec7191e0fce3ca28add6ec53f1
Bakhodir Ulugov <101068416+bulugov@users.noreply.github.com>
2022-04-15 15:02:29 +0400

    Add files via upload

---
+++ 2/functions.py
@@ -0,0 +1,34 @@
+import random
+
+
+class Rectangle:
+    def __init__(self, number, length, width):
+        self.length = length
+        self.width = width
+        self.number = number
+
+    def calculateArea(self):
+
+        return self.length * self.width
+
+
+list = [1, 2, 3, 4]
+
+
+def calculateMinMax():
+    # Creates several objects of class Rectangle and finds min/max areas
+    areas = []
+
+    for i in range(len(list)):
+        list[i] = Rectangle(i, random.randint(1, 50), random.randint(1, 50))
+    for obj in range(len(list)):
+        areas.append(list[obj].calculateArea())
+
+    print(areas)
+    max_area = max(areas)
+    print("The max area is:", max_area)
+    min_area = min(areas)
+    print("The min area is:", min_area)
+
+
+calculateMinMax()
