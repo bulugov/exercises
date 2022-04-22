29619fff2d320c631ea8d8e0a1508211eabe0736
Bakhodir Ulugov <101068416+bulugov@users.noreply.github.com>
2022-04-22 10:58:04 +0400

    Add files via upload

--- 3/main.py
+++ 3/main.py
@@ -1,10 +1,8 @@
 import psycopg2
 import sys
+import re
 
-conn = psycopg2.connect("dbname=testing user=postgres password=6927153a")
-
-cur = conn.cursor()
-
+regex = "^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$"
 
 if len(sys.argv) == 1:
     print("Parameter cannot be empty!")
@@ -16,18 +14,33 @@ else:
     arg = sys.argv[1]
 
 
+def check(email):
+    """
+    Check if user email is valid or not
+    """
+    if re.fullmatch(regex, email):
+        pass
+    else:
+        print("Invalid Email")
+        sys.exit(1)
+
+
 def list_employee():
+    """
+    List all entries in employee table
+    """
+    conn = psycopg2.connect("dbname=testing user=postgres password=6927153a")
+    cur = conn.cursor()
+    try:
+        cur.execute("SELECT first_name, last_name, email, code FROM EMPLOYEE")
 
-    cur.execute("SELECT * FROM EMPLOYEE")
+        record = cur.fetchall()
 
-    record = cur.fetchall()
-
-    for row in record:
-        print("Id = ", row[0])
-        print("First name = ", row[1])
-        print("Last name = ", row[2])
-        print("Email = ", row[3])
-        print("Unique code  = ", row[4], "\n")
+        for row in record:
+            print(("{:<15}" * len(row)).format(*row))
+    finally:
+        cur.close()
+        conn.close()
 
 
 def insert_employee():
@@ -35,69 +48,90 @@ def insert_employee():
     Insert a new entry in employee table
     """
 
-    fname = input("Enter the first name:")
-    lname = input("Enter the last name:")
-    new_email = input("Enter the email:")
-    new_code = input("Enter the code:")
+    conn = psycopg2.connect("dbname=testing user=postgres password=6927153a")
+    cur = conn.cursor()
 
-    cur.execute(
-        "INSERT INTO EMPLOYEE(first_name, last_name, email,code) VALUES(%s, %s, %s, %s);",
-        (
-            fname,
-            lname,
-            new_email,
-            new_code,
-        ),
-    )
+    try:
+        fname = input("Enter the first name:")
+        lname = input("Enter the last name:")
+        new_email = input("Enter the email:")
+        check(new_email)
+        new_code = input("Enter the code:")
 
-    conn.commit()
+        if fname == "" and lname == "" and new_email == "" and new_code == "":
+            print("Empty parameter(s)")
+            sys.exit(1)
 
-    cur.close()
+        cur.execute(f"SELECT * from EMPLOYEE WHERE code='{new_code}'")
+        if cur.fetchall():
+            print("Code is already taken!")
+            sys.exit(1)
 
-    conn.close()
-
-    print("New entry inserted.")
+        cur.execute(
+            "INSERT INTO EMPLOYEE(first_name, last_name, email,code) VALUES(%s, %s, %s, %s);",
+            (
+                fname,
+                lname,
+                new_email,
+                new_code,
+            ),
+        )
+        conn.commit()
+        print("New entry inserted.")
+    finally:
+        cur.close()
+        conn.close()
 
 
 def update_employee():
     """
-    Update a row in employee table
+    Update an entry in employee table
     """
-    code = input("Enter the existing code of the employee:")
-    new_fname = input("Enter the new first name:")
-    new_lname = input("Enter the new last name:")
-    new_email = input("Enter the new email:")
+    conn = psycopg2.connect("dbname=testing user=postgres password=6927153a")
+    cur = conn.cursor()
 
-    if code != "":
+    try:
+        code = input("Enter the existing code of the employee:")
+        new_fname = input("Enter the new first name:")
+        new_lname = input("Enter the new last name:")
+        new_email = input("Enter the new email:")
+        check(new_email)
+
+        if new_fname == "" and new_lname == "" and new_email == "":
+            print("Empty parameter(s)")
+            sys.exit(1)
+
         cur.execute(
             """UPDATE EMPLOYEE SET first_name = %s, last_name = %s, email = %s WHERE code = %s;""",
             (new_fname, new_lname, new_email, code),
         )
-    else:
-        print("Error: empty code!")
-
-    conn.commit()
-
-    cur.close()
-
-    conn.close()
-
-    print(cur.rowcount, "record affected")
+        conn.commit()
+        print("Entry updated.")
+    finally:
+        cur.close()
+        conn.close()
 
 
 def delete_employee():
     """
     Delete a record from the table
     """
+    conn = psycopg2.connect("dbname=testing user=postgres password=6927153a")
+    cur = conn.cursor()
+    try:
+        code = input("Enter the code of the employee to delete:")
 
-    code = input("Enter the code of the employee to delete:")
-    cur.execute("""DELETE FROM EMPLOYEE WHERE CODE = %(str)s;""", {"str": code})
+        if code == "":
+            print("Empty parameter")
+            sys.exit(1)
 
-    conn.commit()
-    cur.close()
-    conn.close()
+        cur.execute("""DELETE FROM EMPLOYEE WHERE CODE = %(str)s;""", {"str": code})
 
-    print(cur.rowcount, "records affected")
+        conn.commit()
+        print("Entry deleted.")
+    finally:
+        cur.close()
+        conn.close()
 
 
 if arg == "list":
